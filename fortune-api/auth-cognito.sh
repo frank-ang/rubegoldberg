fab4368d6b72ba6f5eff2461bf44a6ce8ccf1600
Frank Ang <frankang@amazon.com>
2020-10-31 22:22:17 +0800

    removed hardcoded git token test

--- fortune-api/auth-cognito.sh
+++ fortune-api/auth-cognito.sh
@@ -9,12 +9,9 @@ clientid="$COGNITO_APP_CLIENT_ID"
 token_request=`aws cognito-idp initiate-auth --auth-flow USER_PASSWORD_AUTH --output json --client-id $clientid --auth-parameters USERNAME=$username,PASSWORD=$password`
 response_json=$token_request
 
-echo "Auth Response: $response_json"
-
 AccessToken=`echo $response_json | jq -r ".AuthenticationResult.AccessToken"`
 IdToken=`echo $response_json | jq -r ".AuthenticationResult.IdToken"`
 RefreshToken=`echo $response_json | jq -r ".AuthenticationResult.RefreshToken"`
 ExpiresIn=`echo $response_json | jq -r ".AuthenticationResult.ExpiresIn"`
 
-token=$AccessToken
-echo $token
+echo $AccessToken
